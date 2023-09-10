e9690b67b9167a15dfdb2a17cc44b7fdb20791d4
lavu <gcpbigdatab@gmail.com>
2023-09-10 17:47:13 +0530

    did some changes required

--- Cloud-Functions/http-triggers/Factorial-using-http-trigger/main.py
+++ Cloud-Functions/http-triggers/Factorial-using-http-trigger/main.py
@@ -1,19 +1,9 @@
-# main.py
-
-def calculate_factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * calculate_factorial(n-1)
-
 def factorial(request):
-    number = request.args.get('number')
-    if number is not None:
-        try:
-            number = int(number)
-            result = calculate_factorial(number)
-            return f"Factorial of {number} is {result}"
-        except ValueError:
-            return "Invalid input. Please provide a valid number."
+    user_input = request.args.get('input')
+    if user_input is not None:
+        userid, password = user_input.split()
+    # ... Your code logic ...
+        return "Success"  # Or your desired response
     else:
-        return "Please provide a 'number' query parameter."
+        return "Invalid input. Please provide a valid 'input' parameter."
+

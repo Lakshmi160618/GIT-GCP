74261134f018a3388651bef4173e6e7f245f147b
lavu <gcpbigdatab@gmail.com>
2023-09-11 13:13:44 +0530

    this is the  code to save the bq query result into google drive using python

--- BigQuery/bq_to_gdrive_using_pyspark/main.py
+++ BigQuery/bq_to_gdrive_using_pyspark/main.py
@@ -1,8 +1,9 @@
-from pandas import read_csv, DataFrame
 from google.oauth2.service_account import Credentials
+from google.cloud import bigquery
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 import pandas as pd
+from google.cloud import bigquery
 
 # Google Drive API scope
 SCOPES = ["https://www.googleapis.com/auth/drive"]
@@ -21,23 +22,31 @@ service = build(
     cache_discovery=False,
 )
 
-# Create or load your Pandas DataFrame (replace this with your data)
-data = {
-    'Name': ['Alice', 'Bob', 'Charlie'],
-    'Age': [25, 30, 35],
-    'City': ['New York', 'San Francisco', 'Los Angeles']
-}
+# Initialize BigQuery client with your credentials
+'''bigquery_client = bigquery.Client.from_service_account_json(
+    "/home/sudheerrbl5256/secrets/divine-command-396711-6e2f9a7411ba.json"
+)'''
+bigquery_client = bigquery.Client()
+# Define your BigQuery SQL query
+sql_query = """
+SELECT
+     *
+FROM
+    gcp-demo-dev-397415.Lakshmi.Accounts
+"""
 
-df = pd.DataFrame(data)
-
-# Specify the file path for the CSV file
-csv_file_path = '/home/sudheerrbl5256/my_dataframe.csv'
-
-# Save the DataFrame to the CSV file
-df.to_csv(csv_file_path, index=False)  # Set index=False to exclude the index column
+# Execute the BigQuery SQL query and convert the result to a DataFrame
+query_job = bigquery_client.query(sql_query)
+df = query_job.to_dataframe()
 
 # Specify the folder ID in Google Drive where you want to upload the file
 folder_id = '1pjlonEKhMaWb35ICtTFeT3RhoV5xYqgw'
+
+# Specify the file path for the CSV file
+csv_file_path = '/home/sudheerrbl5256/my_dataframebq.csv'
+
+# Save the DataFrame to the CSV file
+df.to_csv(csv_file_path, index=False)
 
 # Create a media upload object
 media = MediaFileUpload(
@@ -47,7 +56,7 @@ media = MediaFileUpload(
 
 # Create the file in Google Drive with the specified parent folder
 file_metadata = {
-    'name': 'my_dataframe.csv',
+    'name': 'my_dataframebq.csv',
     'parents': [folder_id]
 }
 

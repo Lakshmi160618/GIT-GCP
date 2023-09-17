98c6a6e388d047b81c8a9467b6d19e16d7124a62
lavu <gcpbigdatab@gmail.com>
2023-09-17 21:47:32 +0530

    slack webhook is changed

--- Google-Cloud-Functions/Slack and GITHUB Integration/main.py
+++ Google-Cloud-Functions/Slack and GITHUB Integration/main.py
@@ -3,12 +3,10 @@ from requests.structures import CaseInsensitiveDict
 
 def my_func(request):
 	print("slack with gitwebhooks")
-	url = "https://hooks.slack.com/services/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"
+	url = "https://hooks.slack.com/services/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"
 	headers = CaseInsensitiveDict()
 	headers["Content-type"]= "application/json"
 	data = '{"text":"GITHUB code is committed"}'
 	resp = requests.post(url,headers=headers, data=data)
 	print(resp.status_code)
-	return f"Hellow World"
-
-my_func()
+	return f"Hellow World" 

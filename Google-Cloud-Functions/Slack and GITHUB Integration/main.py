5abebfbfa40eb0c613fcc1f600513fd3e035c1d3
lavu <gcpbigdatab@gmail.com>
2023-09-18 09:12:28 +0530

    it is working now

--- Google-Cloud-Functions/Slack and GITHUB Integration/main.py
+++ Google-Cloud-Functions/Slack and GITHUB Integration/main.py
@@ -1,12 +1,13 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 
-def my_func(request):
+def my_func():
 	print("slack with gitwebhooks")
-	url = "https://hooks.slack.com/services/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"
+	url = "https://hooks.slack.com/services/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"
 	headers = CaseInsensitiveDict()
 	headers["Content-type"]= "application/json"
 	data = '{"text":"GITHUB code is committed"}'
 	resp = requests.post(url,headers=headers, data=data)
 	print(resp.status_code)
 	return f"Hellow World" 
+my_func()

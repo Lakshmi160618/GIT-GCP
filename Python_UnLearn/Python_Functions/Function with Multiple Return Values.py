dba67ce1dfe114a8a3e541be5ce609cd198a8da3
lavu <gcpbigdatab@gmail.com>
2023-09-20 16:21:40 +0530

    small change

--- Python_UnLearn/Python_Functions/Function with Multiple Return Values.py
+++ Python_UnLearn/Python_Functions/Function with Multiple Return Values.py
@@ -5,10 +5,19 @@ def get_name_and_age():
 
 # Call the function
 result = get_name_and_age()
-name, age = result
+name, age = result # tuple unpacking
 print("Name:", name)
 print("Age:", age)
 
 '''
 In this example, the get_name_and_age function returns two values, name and age, as a tuple. When called, you can unpack the returned tuple into separate variables.
 '''
+def even_or_odd(num):
+    if num%2==0:
+        return "even number"
+    else:
+        return "odd number"
+
+for x in range(10): # 10 times
+    result = even_or_odd(x) # it is the line from which you are calling the function
+    print(x,result)

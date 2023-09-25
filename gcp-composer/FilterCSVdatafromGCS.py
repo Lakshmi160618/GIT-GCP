c26a358f93cd8a722fe4c0ba7dcc3bdb69e035c0
lavu <gcpbigdatab@gmail.com>
2023-09-25 06:57:30 +0530

    some change added as to process multiple csv files in the same folder

--- gcp-composer/FilterCSVdatafromGCS.py
+++ gcp-composer/FilterCSVdatafromGCS.py
@@ -6,12 +6,12 @@ from airflow.operators.python_operator import PythonOperator
 
 # Define your GCS bucket and file paths
 bucket_name = 'lakshmi_2023'
-source_blob_name = 'raw/Customers.csv'
-destination_blob_name = 'filtered/Customers.csv'
+source_blob_name = 'raw/'
+destination_blob_name = 'filtered/'
 
 # Define your Airflow DAG
 default_args = {
-    'owner': 'your_name',
+    'owner': 'airflow',
     'start_date': days_ago(1),
 }
 
@@ -27,16 +27,29 @@ dag = DAG(
 # Task to process the CSV file
 def process_csv():
     # Initialize the GCS client
-    storage_client = storage.Client()
-    
-    # Read the CSV file directly from GCS into a Pandas DataFrame
-    df = pd.read_csv(f'gs://{bucket_name}/{source_blob_name}')
-    
-    # Select only the first 37 columns
-    selected_columns = df.iloc[:, :37]
-    
-    # Write the selected data as a CSV file to a GCS location using df.to_csv
-    selected_columns.to_csv(f'gs://{bucket_name}/{destination_blob_name}', index=False)
+	storage_client = storage.Client()
+	
+	# List all the CSV files in the source folder
+	blobs = storage_client.list_blobs(bucket_name, prefix=source_folder)
+	
+	# Loop through each CSV file and process it
+	for blob in blobs:
+		if blob.name.endswith('.csv'):
+			# Read the CSV file directly from GCS into a Pandas DataFrame
+			df = pd.read_csv(f'gs://{bucket_name}/{blob.name}')
+			
+			# Select only the first 37 columns
+			selected_columns = df.iloc[:, :37]
+			
+			# Define the destination blob name in the filtered folder
+			destination_blob_name = blob.name.replace(source_folder, destination_folder)
+			
+			# Write the selected data as a CSV file to a GCS location using df.to_csv
+			selected_columns.to_csv(f'gs://{bucket_name}/{destination_blob_name}', index=False)
+			
+			print(f'Selected columns saved to: gs://{bucket_name}/{destination_blob_name}')
+	
+
 
 # PythonOperator to process the CSV
 process_csv_task = PythonOperator(

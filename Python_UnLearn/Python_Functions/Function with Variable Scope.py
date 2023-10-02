f6a7e5c6fe6ff3d55c16fa87f1b2664ed7ef3138
lavu <gcpbigdatab@gmail.com>
2023-10-02 08:26:11 +0530

    new change is added

--- Python_UnLearn/Python_Functions/Function with Variable Scope.py
+++ Python_UnLearn/Python_Functions/Function with Variable Scope.py
@@ -13,3 +13,18 @@ outer_function()
 '''
 This example demonstrates variable scope within nested functions. The inner_function modifies the value of a variable declared in the outer function using the nonlocal keyword.
 '''
+# one more example
+x = 10
+print(x)
+def func():
+    x = 20 # Enclosed plac variable
+    print(x)
+    def inner_func1():
+        x = 30 # non local
+        print(x)
+        return x
+    res = inner_func1()
+    return res
+
+x = func()
+print(x) 
--- Python_UnLearn/Python_Functions/Lambda Function.py
+++ Python_UnLearn/Python_Functions/Lambda Function.py
@@ -3,7 +3,10 @@ add = lambda x, y: x + y
 # Use the lambda function
 result = add(5, 3)
 print("Sum:", result)
-
+list = [2,3,4,3,4,5,6]
+# with out using set function you need to remove all the dupes from the list
+print(list(set(list)))
+# you need to sort this list without using sort method
 """
 In this example, a lambda function add is defined, which takes two parameters and returns their sum. Lambda functions are typically used for small, simple operations.
 """
